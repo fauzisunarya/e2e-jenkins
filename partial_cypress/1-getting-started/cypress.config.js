58fc3fd7e1807265d8e4f500b6d28415fbcc9120
@neuron_fauzi <fauzi.sunarya@neuronworks.co.id>
2024-10-04 13:52:37 +0700

    html report

--- partial_cypress/1-getting-started/cypress.config.js
+++ partial_cypress/1-getting-started/cypress.config.js
@@ -1,12 +1,24 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir:'cypress/report',
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    debug: true
+  },
   e2e: {
     specPattern: [
-        "cypress/e2e/1-getting-started/*.cy.js",
-      ],
+      "cypress/e2e/1-getting-started/*.cy.js"
+    ],
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    require('cypress-mochawesome-reporter/plugin')(on);
+    return config;
     },
   },
 });
--- partial_cypress/2-advanced-examples/cypress.config.js
+++ partial_cypress/2-advanced-examples/cypress.config.js
@@ -1,12 +1,24 @@
 const { defineConfig } = require("cypress");
 
+
 module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    reportDir:'cypress/report',
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+    debug: true
+  },
   e2e: {
     specPattern: [
-        "cypress/e2e/2-advanced-examples/*.cy.js",
-      ],
+      "cypress/e2e/2-advanced-examples/*.cy.js"
+    ],
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+    require('cypress-mochawesome-reporter/plugin')(on);
+    return config;
     },
   },
 });
