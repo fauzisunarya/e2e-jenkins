77c1a0e45b74158ca5df98d198d1cb3d2460ee6c
@neuron_fauzi <fauzi.sunarya@neuronworks.co.id>
2024-10-04 11:26:30 +0700

    partial

--- partial_cypress/1-getting-started/cypress.config.js
+++ partial_cypress/1-getting-started/cypress.config.js
@@ -3,9 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     specPattern: [
-      "cypress/e2e/1-getting-started/*.cy.js",
-      "cypress/e2e/2-advanced-examples/*.cy.js"
-    ],
+        "cypress/e2e/1-getting-started/*.cy.js",
+      ],
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
